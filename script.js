cb4aaa099baa47972348783301a4d18ba078fb4d
Eliezer Nsengi <nsengitech@gmail.com>
2024-04-18 15:14:09 +0200

    error: removed the . functionality
    to be fixed tomorrow

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const main = document.querySelector('main')
-let input = document.querySelector('textarea')
+let input = document.querySelector('textarea')  
 const audio = document.querySelector(`audio[data-key="1"]`)
+
 input.focus()
 main.addEventListener('click', (e)=>{
      if (e.target.tagName == 'BUTTON'){
-        audio.play()  
-        if (e.target.className == 'submit')
+            audio.play()  
+        if (e.target.className == 'submit'){
         return eval(input.textContent)? input.innerText= eval(input.textContent): input.innerText= ""
+    }
         if(e.target.className == 'reset') return input.innerText=''
-        if(e.target.className.includes('operator')){
-            if(e.target.className.includes('dot')){
-                if((input.textContent).includes(".")) return input.innerText = eval(input.textContent)
+        if(e.target.className.includes('operator') || e.target.className.includes('dot')){
+if(e.target.className.includes('dot')){
+                if((input.textContent).includes(".")) return
                 }
             if("+-/.*".includes(input.textContent[input.textContent.length-1] )){
             input.innerText = eval(input.textContent.substring(0,input.textContent.length -1)) + e.target.innerText
